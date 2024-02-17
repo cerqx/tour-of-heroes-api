3870bc5e7053b757306970eed57e404aafd24f1e
Cerqueira <contatocerq@gmail.com>
2024-02-17 17:51:18 -0300

    Search from index

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,4 +2,5 @@ class Hero < ApplicationRecord
     validates :name, presence: true
 
     scope :sorted_by_name, -> { order(:name) }
+    scope :search, ->(name) { where('LOWER(name) LIKE ?', "%#{name.downcase}%") if name.present? }
 end
